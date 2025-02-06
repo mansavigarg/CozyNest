f936f037349568752110465747077df4236653de
mansavigarg <mansavigarg9919@gmail.com>
2025-02-06 16:11:06 +0530

    GenderCollectionSection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import UserLayout from "./components/Layout/UserLayout"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<UserLayout />}> {/* User Layout */} </Route>
-        <Route >{/* Admin Layout */}</Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<UserLayout />}>
+            {/* User Layout */}
+            <Route index element={<Home />}></Route>
+          </Route>
+          <Route>{/* Admin Layout */}</Route>
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

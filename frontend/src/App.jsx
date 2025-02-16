c01faa2e2f3f9c0c52457e715bac457dd9ad1e03
mansavigarg <mansavigarg9919@gmail.com>
2025-02-16 10:03:09 +0530

    added login register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<UserLayout />}>
             {/* User Layout */}
-            <Route index element={<Home />}></Route>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
           </Route>
           <Route>{/* Admin Layout */}</Route>
         </Routes>

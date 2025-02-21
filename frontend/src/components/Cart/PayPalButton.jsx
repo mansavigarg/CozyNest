1855d510f0ece312f1c95fa107174c86598eb85e
mansavigarg <mansavigarg9919@gmail.com>
2025-02-21 23:03:26 +0530

    checkout page

---
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+const PayPalButton = ({ amount, onSuccess, onError }) => {
+  return (
+    <PayPalScriptProvider options={{ "client-id": "ASi9bgKf-LlvXEtEiCKsb8sYL_BQGHNE5IV-Kr05coGY0_8UqVLSaB4d-NjdlW5gI1NEhYypXbhYtWub" }}>
+      <PayPalButtons
+        style={{ layout: "vertical" }}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [{ amount: { value: amount } }],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then(onSuccess);
+        }}
+        onError={onError}
+      />
+    </PayPalScriptProvider>
+  );
+};
+
+export default PayPalButton;
--- frontend/src/components/Layout/CardDrawer.jsx
+++ frontend/src/components/Layout/CardDrawer.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io"
 import CartContents from "../Cart/CartContents";
+import { useNavigate } from "react-router-dom";
 
 const CardDrawer = ({drawerOpen,toggleCartDrawer}) => {
 
+
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    navigate("/checkout")
+  }
 
   return (
     <div
@@ -27,7 +33,7 @@ const CardDrawer = ({drawerOpen,toggleCartDrawer}) => {
 
       {/* Checkout button fixed at bottom */}
       <div className=" p-4 bg-white sticky bottom-0">
-        <button className=" w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">Checkout </button>
+        <button onClick={handleCheckout} className=" w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">Checkout </button>
         <p className=" text-sm tracking-tighter text-gray-500 mt-2 text-center"> 
           Shipping, taxes and discount codes calculated at checkout.
         </p>
